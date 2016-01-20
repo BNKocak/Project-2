cdb5c39217bfd4bd061cb21084d21d9b2e01c77a
Mustafa Kocakaya <kocakaya_35@hotmail.com>
2016-01-20 12:01:25 +0100

    v0.12

--- Project 2/Project_2.py
+++ Project 2/Project_2.py
@@ -1,4 +1,4 @@
-﻿import time
+import time
 from threading import Thread
 import os, pygame
 import time
@@ -8,8 +8,7 @@ size = width, height = 1700, 1080
 black = 0, 0, 0
 white = 255, 255, 255
 green = 50, 255, 100
-#screen = pygame.display.set_mode(size,)
-screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+screen = pygame.display.set_mode(size)
 offset = 50
 size = 10
 
@@ -27,11 +26,8 @@ play_r.x, play_r.y = (705, 360)
 exit_r = exit.get_rect()
 exit_r.x, exit_r.y = (705, 460)
 def Main():
-  play_r = play.get_rect()
-  play_r.x, play_r.y = (705, 360)
   start = time.time()
   playing = False
-  players = False
   currentscreen = 0
   Hover_play_r = False
   Hover_exit_r = False
@@ -41,7 +37,7 @@ def Main():
     # 1 = After menu > amount players
     # 2 = Board game
     # 3 = Pause menu
-    if playing == False:
+    if playing == False and currentscreen == 0:                     # HOVER OVER BUTTONS
         if Hover_play_r == True:
             screen.blit(play_hover, (705,360))
             currentscreen = 0
@@ -54,8 +50,8 @@ def Main():
             screen.blit(play, (705,360))
             screen.blit(exit, (705,460))
             currentscreen = 0
-    for event in pygame.event.get():
-        if play_r.collidepoint(pygame.mouse.get_pos()):
+    for event in pygame.event.get():                                # for loop EVENT iteration
+        if play_r.collidepoint(pygame.mouse.get_pos()):             # HOVER CHECK
             Hover_play_r = True
         else:
             Hover_play_r = False
@@ -63,38 +59,50 @@ def Main():
             Hover_exit_r = True
         else:
             Hover_exit_r = False
-        if event.type == pygame.MOUSEBUTTONUP:
+        if currentscreen == 2:
+            screen.fill((255, 0, 255))
+            screen.blit(board, (0,0))
+        if event.type == pygame.MOUSEBUTTONUP:                      # EVEN PROCESSING MOUSECLICKS
             pos = pygame.mouse.get_pos()
             pos_x = pos[0]
             pos_y = pos[1]
             print(pos_x)
             print(pos_y)
-            if (pos_x > 704) and (pos_x < 960) and (pos_y > 359) and (pos_y < 406):
+            if (pos_x > 704) and (pos_x < 960) and (pos_y > 359) and (pos_y < 406) and currentscreen == 0:
                 print("PLAY")
                 playing = True
                 if playing == True and currentscreen == 0:
-                    screen.blit(menu, (0,0))
+                    screen.fill(black)
                     pygame.font.init()
                     myfont = pygame.font.SysFont("monospace", 30)
                     label = myfont.render("Choose amount of players.", 1, (255,255,255))
+                    screen.blit(menu, (0,0))
+                    screen.blit(label, (425, 200))
                     screen.blit(number2, (705,360))
                     screen.blit(number3, (705,460))
                     screen.blit(number4, (705,560))
-                    screen.blit(label, (425, 200))
                     currentscreen = 1
-            elif (pos_x > 704) and (pos_x < 960) and (pos_y > 459) and (pos_y < 506):
-                if currentscreen == 0:
-                    pygame.quit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
+            elif (pos_x > 704) and (pos_x < 960) and (pos_y > 459) and (pos_y < 506) and currentscreen == 0:
+                print("QUIT")
+                pygame.quit()
+            elif (pos_x > 704) and (pos_x < 961) and (pos_y > 359) and (pos_y < 403):
                 if currentscreen == 1:
                     screen.blit(board, (0,0))
-        if event.type == pygame.KEYDOWN:
+                    currentscreen = 2
+            elif (pos_x > 704) and (pos_x < 961) and (pos_y > 359) and (pos_y < 503):
+                if currentscreen == 1:
+                    screen.blit(board, (0,0))
+                    currentscreen = 2
+            elif (pos_x > 704) and (pos_x < 961) and (pos_y > 359) and (pos_y < 603):
+                if currentscreen == 1:
+                    screen.blit(board, (0,0))
+                    currentscreen = 2
+        if event.type == pygame.KEYDOWN:                                                # PAUSE SCREEN
             if event.key == pygame.K_ESCAPE:
                 playing == False
                 screen.fill(black)
                 screen.blit(play, (705,360))
-                screen.blit(quit, (705,460))
+                screen.blit(exit, (705,460))
                 currentscreen = 3
 
         #elif event.type == pygame.MOUSEMOTION:
