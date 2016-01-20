799e4253d5650b3592a32b95d9ac4160446ebc6d
Mustafa Kocakaya <kocakaya_35@hotmail.com>
2016-01-20 13:11:11 +0100

    v0.13

--- Project 2/Project_2.py
+++ Project 2/Project_2.py
@@ -1,10 +1,11 @@
-import time
+﻿import time
 from threading import Thread
 import os, pygame
 import time
+import random
 
 pygame.init()
-size = width, height = 1700, 1080
+size = width, height = 1700, 950
 black = 0, 0, 0
 white = 255, 255, 255
 green = 50, 255, 100
@@ -12,6 +13,8 @@ screen = pygame.display.set_mode(size)
 offset = 50
 size = 10
 
+
+#---------------------------------------------------------------------------
 board = pygame.image.load("Content/Board.jpg")
 menu = pygame.image.load("Content/Menu.jpg") 
 play = pygame.image.load("Content/Play.png").convert()
@@ -21,16 +24,29 @@ exit_hover = pygame.image.load("Content/Quit_hover.png").convert()
 number2 = pygame.image.load("Content/#1.png").convert()
 number3 = pygame.image.load("Content/#2.png").convert()
 number4 = pygame.image.load("Content/#3.png").convert()
+dice_1 = pygame.image.load("Content/1.png").convert()
+dice_2 = pygame.image.load("Content/2.png").convert()
+dice_3 = pygame.image.load("Content/3.png").convert()
+dice_4 = pygame.image.load("Content/4.png").convert()
+dice_5 = pygame.image.load("Content/5.png").convert()
+dice_6 = pygame.image.load("Content/6.png").convert()
+dice = pygame.image.load("Content/dice.png").convert()
+
 play_r = play.get_rect()
 play_r.x, play_r.y = (705, 360)
 exit_r = exit.get_rect()
 exit_r.x, exit_r.y = (705, 460)
+dice_r = dice.get_rect()
+dice_r.x, dice_r.y = (1180, 300)
+dicelist = [dice_1, dice_2, dice_3, dice_4, dice_5, dice_6]
+#----------------------------------------------------------------------------
 def Main():
   start = time.time()
   playing = False
   currentscreen = 0
   Hover_play_r = False
   Hover_exit_r = False
+  diceclicked = False
   while True:    
     pygame.event.pump() 
     # 0 = First Menu
@@ -60,8 +76,10 @@ def Main():
         else:
             Hover_exit_r = False
         if currentscreen == 2:
-            screen.fill((255, 0, 255))
-            screen.blit(board, (0,0))
+            if diceclicked == False:
+                screen.fill(black)
+                screen.blit(board, (0,0))
+                screen.blit(dice, (1180, 300))
         if event.type == pygame.MOUSEBUTTONUP:                      # EVEN PROCESSING MOUSECLICKS
             pos = pygame.mouse.get_pos()
             pos_x = pos[0]
@@ -75,7 +93,7 @@ def Main():
                     screen.fill(black)
                     pygame.font.init()
                     myfont = pygame.font.SysFont("monospace", 30)
-                    label = myfont.render("Choose amount of players.", 1, (255,255,255))
+                    label = myfont.render("Choose amount of players", 1, (255,255,255))
                     screen.blit(menu, (0,0))
                     screen.blit(label, (425, 200))
                     screen.blit(number2, (705,360))
@@ -87,16 +105,18 @@ def Main():
                 pygame.quit()
             elif (pos_x > 704) and (pos_x < 961) and (pos_y > 359) and (pos_y < 403):
                 if currentscreen == 1:
-                    screen.blit(board, (0,0))
                     currentscreen = 2
             elif (pos_x > 704) and (pos_x < 961) and (pos_y > 359) and (pos_y < 503):
                 if currentscreen == 1:
-                    screen.blit(board, (0,0))
                     currentscreen = 2
             elif (pos_x > 704) and (pos_x < 961) and (pos_y > 359) and (pos_y < 603):
                 if currentscreen == 1:
-                    screen.blit(board, (0,0))
                     currentscreen = 2
+            elif dice_r.collidepoint(pygame.mouse.get_pos()):
+                i = random.randint(0, 5)
+                screen.blit(dicelist[i], (1180, 300))
+                diceclicked = True
+
         if event.type == pygame.KEYDOWN:                                                # PAUSE SCREEN
             if event.key == pygame.K_ESCAPE:
                 playing == False
